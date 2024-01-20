7bcebd19f279780587bf15606c6ba3f8ae7e315c
lrosati18 <lilianarosati@gmail.com>
2024-01-20 13:55:20 -0500

    completed Main section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,38 +1,45 @@
-// import { useState } from "react";
-// import videoInfo from "../../data/video-details.json";
 import "./Main.scss";
+import viewsIcon from "../../assets/images/icons/views.svg";
+import likesIcon from "../../assets/images/icons/likes.svg";
 
 function Main({ selectedVideo }) {
-  // const [video, setVideo] = useState(videoInfo[0]);
+  function toDate(timestamp) {
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleDateString("en-US", {
+      timeZone: "UTC",
+      month: "2-digit",
+      day: "2-digit",
+      year: "numeric",
+    });
 
-  // const changeVideo = (videoId) => {
-  //   const newVideo = videoInfo.find((video) => video.id === videoId);
-  //   setVideo(newVideo);
-  // };
-
-  // function toDate(timestamp) {
-  //   const date = new Date(timestamp);
-  //   const formattedDate = date.toLocaleDateString("en-US", {
-  //     timeZone: "UTC",
-  //     month: "2-digit",
-  //     day: "2-digit",
-  //     year: "numeric",
-  //   });
-
-  //   return formattedDate;
-  // }
+    return formattedDate;
+  }
 
   return (
     <section className="main">
       <div className="main__container">
-        <img className="main__image" src={selectedVideo.image} />
+        <video
+          className="main__image"
+          controls
+          poster={selectedVideo.image}
+        ></video>
       </div>
       <h1 className="main__title">{selectedVideo.title}</h1>
       <div className="main__info">
-        <p className="main__channel">By {selectedVideo.channel}</p>
-        <p className="main__date">{toDate(selectedVideo.timestamp)}</p>
-        <p className="main__views">{selectedVideo.views}</p>
-        <p className="main__likes">{selectedVideo.likes}</p>
+        <div className="main__wrapper">
+          <p className="main__channel">By {selectedVideo.channel}</p>
+          <p className="main__date">{toDate(selectedVideo.timestamp)}</p>
+        </div>
+        <div className="main__wrapper">
+          <div className="views">
+            <img className="views__icon" src={viewsIcon} alt="views icon"></img>
+            <p className="views__text">{selectedVideo.views}</p>
+          </div>
+          <div className="likes">
+            <img className="likes__icon" src={likesIcon} alt="likes icon"></img>
+            <p className="likes__text">{selectedVideo.likes}</p>
+          </div>
+        </div>
       </div>
       <p className="main__description">{selectedVideo.description}</p>
     </section>
