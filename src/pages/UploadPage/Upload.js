ab2276063d5a6203adc5f9cc5f33c8d07116aef4
lrosati18 <lilianarosati@gmail.com>
2024-01-24 22:10:26 -0500

    finish Upload page

--- src/pages/UploadPage/Upload.js
+++ src/pages/UploadPage/Upload.js
@@ -1,19 +1,7 @@
-export default function Upload() {
-  return (
-    <section className="upload">
-      <h1>Upload Video</h1>
-      <form>
-        <label></label>
-        <img></img>
-        <label></label>
-        <input></input>
-        <label></label>
-        <input></input>
-        {/* onClick this must notify that video has been uploaded and route back to homepage */}
-        <button type="submit"></button>
-        {/* This may be a button functionality later */}
-        <p>Cancel</p>
-      </form>
-    </section>
-  );
+import UploadForm from "../../components/UploadForm/UploadForm";
+
+function Upload() {
+  return <UploadForm />;
 }
+
+export default Upload;
