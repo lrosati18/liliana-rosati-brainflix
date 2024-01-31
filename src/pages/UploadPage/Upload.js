c43221bc7376aa2f891ca9692fa03f2c5a2a3708
lrosati18 <lilianarosati@gmail.com>
2024-01-30 19:38:20 -0500

    preliminary setup

--- src/pages/UploadPage/Upload.js
+++ src/pages/UploadPage/Upload.js
@@ -6,6 +6,12 @@ function Upload() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    //upload functionality
+    //should i be using refs?
+    const form = event.target;
+    const title = form.title.value;
+    const description = form.description.value;
+
     alert("Your upload was successful!");
     navigate("/");
   };
